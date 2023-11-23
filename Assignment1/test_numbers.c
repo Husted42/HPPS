3ef26a35e46f6ce298c3a437341bc656df875bb1
husted42 <alx2608@gmail.com>
2023-11-23 16:48:27 +0100

    iskold

--- Assignment1/test_numbers.c
+++ Assignment1/test_numbers.c
@@ -2,5 +2,11 @@
 #include "numbers.h"
 
 int main() {
-  // TODO
+  // unsigned int x = 11;
+  // unsigned int b = bits8_to_int(bits8_from_int(x));
+  // bits8_print(bits8_from_int(x));
+  unsigned int jens = bits8_to_int(bits8_add(bits8_from_int(9), bits8_from_int(11)));
+  bits8_print(bits8_add(bits8_from_int(9), bits8_from_int(11)));
+  printf("\n%d\n", jens);
+  return 0;
 }
