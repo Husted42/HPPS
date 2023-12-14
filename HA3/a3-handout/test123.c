c55381f70c8826459a4eb352bc8c0cd9e57b7966
Husted42 <alx2608@gmail.com>
2023-12-14 18:06:28 +0100

    alsdkmsalkd

--- HA3/a3-handout/test123.c
+++ HA3/a3-handout/test123.c
@@ -4,3 +4,28 @@
 #include <stdint.h>
 #include <assert.h>
 
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+    exit(1);
+  }
+
+    // Set pointer *f to file, such that we can call f later
+  FILE *f = fopen(argv[1], "r");
+  assert(f != NULL);
+
+  int n, d;
+  // nxd matrix of points
+  double *points = read_points(f, &n, &d);
+  assert(points != NULL);
+    printf("d: %d\n", d);
+  for (int i = 0; i < n; i++) {
+    printf("Point %d: ", i);
+    for (int j = 0; j < d; j++) {
+      printf("%f ", points[i*d+j]);
+    }
+    printf("\n");
+  }
+
+  free(points);
+}
--- exercises/ea3/printpoints.c
+++ exercises/ea3/printpoints.c
@@ -6,27 +6,28 @@
 #include <time.h>
 
 int main(int argc, char** argv) {
-    (void)argc;
-    (void)argv;
-//   if (argc != 2) {
-//     fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-//     exit(1);
-//   }
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+    exit(1);
+  }
 
-//   FILE *f = fopen(argv[1], "r");
-//   assert(f != NULL);
+    // Set pointer *f to file, such that we can call f later
+  FILE *f = fopen(argv[1], "r");
+  assert(f != NULL);
 
-//   int n, d;
-//   double *points = read_points(f, &n, &d);
-//   assert(points != NULL);
+  int n, d;
+  // nxd matrix of points
+  double *points = read_points(f, &n, &d);
+  assert(points != NULL);
+    printf("d: %d\n", d);
+  for (int i = 0; i < n; i++) {
+    printf("Point %d: ", i);
+    for (int j = 0; j < d; j++) {
+      printf("%f ", points[i*d+j]);
+    }
+    printf("\n");
+  }
 
-//   for (int i = 0; i < n; i++) {
-//     printf("Point %d: ", i);
-//     for (int j = 0; j < d; j++) {
-//       printf("%f ", points[i*d+j]);
-//     }
-//     printf("\n");
-//   }
-
-//   free(points);
+  free(points);
 }
+
