9d89fb101156a69fb8db6b9079417ec329239034
AblaZe42 <118982447+AblaZe42@users.noreply.github.com>
2024-01-10 14:48:45 +0100

    hej

--- HA6/heat-equation.c
+++ HA6/heat-equation.c
@@ -5,18 +5,21 @@
 #include <assert.h>
 #include <omp.h>
 #include "debugbmp.h"
+#include "timing.h"
 
 size_t pos(size_t width, size_t x, size_t y) {
     return y*width + x;
 }
 
 void write_borders(float* data, size_t width, size_t height) {
-    unsigned long i;
-    for (i = 1; i < width; ++i){
+    size_t i;
+    //#pragma omp parallel for
+    for (i = 0; i < width; ++i){
         data[pos(width, i, 0)] = 20.0f;
         data[pos(width, i, height-1)] = -273.15f;
     }
-    for (i = 1; i < height; ++i){
+    //#pragma omp parallel for
+    for (i = 0; i < height; ++i){
         data[pos(width, 0, i)] = -273.15f;
         data[pos(width, width-1, i)] = -273.15f;
     }
@@ -26,33 +29,32 @@ float stencil(float* data, size_t width, size_t x, size_t y, float alpha) {
     return alpha *  (data[pos(width, x-1, y)] +
                     data[pos(width, x+1, y)] + 
                     data[pos(width, x, y-1)] + 
-                    data[pos(width, x, y+1)]) + 
-                    (1 - 4 * alpha) * 
-                    data[pos(width, x, y)];
+                    data[pos(width, x, y+1)] + 
+                    data[pos(width, x, y)]);
 }
 
 void apply_stencil(float* data, size_t width, size_t height, size_t offset, float alpha) {
-    // (void)offset;
-    unsigned long x;
-    unsigned long y;
-    for (x=1; x < width-1; ++x){
-        for (y=1+offset; y < height-1; ++y){
+    size_t x, y;
+    //#pragma omp parallel for collapse(1)
+    for (x = 1; x < width - 1; ++x) {
+        for (y = 1 + ((x + offset) % 2); y < height - 1; y += 2) {
             data[pos(width, x, y)] = stencil(data, width, x, y, alpha);
         }
     }
 }
 
 float compute_delta(float* data, float* prev, size_t width, size_t height) {
-    unsigned long i;
-    unsigned long j;
+    size_t i, j;
     float delta = 0.0f;
-    for (i=1; i < width-1; ++i){
-        for (j=1; j < height-1; ++j){
+    //#pragma omp parallel for reduction(+:delta) collapse(2)
+    for (i = 0; i < width; ++i) {
+        for (j = 0; j < height; ++j) {
             delta += fabs(data[pos(width, i, j)] - prev[pos(width, i, j)]);
         }
     }
-    return delta;
+    return delta / (width * height);
 }
+
 
 void run_simulation(size_t width, size_t height, size_t steps, const char* filename) {
     size_t size = width*height;
@@ -107,9 +109,10 @@ int main(int argc, char** argv) {
     if (argc == 5) {
         filename = argv[4];
     }
-
+    double time_start = seconds();
     run_simulation((size_t)width, (size_t)height, (size_t)steps, filename);
-
+    double time_end = seconds();
+    printf("Time: %f\n", time_end - time_start);
     return 0;
 }
 
