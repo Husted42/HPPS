39e798ad92219cb94bd2494e7c61461b926a7807
skywong21 <lgz282@alumni.ku.dk>
2024-01-10 12:45:16 +0100

    .........

--- HA6/heat-equation.c
+++ HA6/heat-equation.c
@@ -6,52 +6,47 @@
 #include <omp.h>
 #include "debugbmp.h"
 
-// Function to calculate position
 size_t pos(size_t width, size_t x, size_t y) {
-    return y * width + x;
+    return y*width + x;
 }
 
-// Function to write borders
 void write_borders(float* data, size_t width, size_t height) {
-    #pragma omp parallel for 
-    for (size_t y = 0; y < height; y++) {
-        for (size_t x = 0; x < width; x++) {
-            if (x == 0 || y == 0 || x == width - 1 || y == height - 1) {
-                data[pos(width, x, y)] = 1.0f;
-            }
-        }
+    unsigned long i;
+    for (i = 1; i < width; ++i){
+        data[pos(width, i, 0)] = 20.0f;
+        data[pos(width, i, height-1)] = -273.15f;
+    }
+    for (i = 1; i < height; ++i){
+        data[pos(width, 0, i)] = -273.15f;
+        data[pos(width, width-1, i)] = -273.15f;
     }
 }
 
-// Function to apply stencil
 float stencil(float* data, size_t width, size_t x, size_t y, float alpha) {
-    float t = data[pos(width, x, y)];
-    t += alpha * (data[pos(width, x+1, y)] - 2*data[pos(width, x, y)] + data[pos(width, x-1, y)]);
-    t += alpha * (data[pos(width, x, y+1)] - 2*data[pos(width, x, y)] + data[pos(width, x, y-1)]);
-    return t;
+    return alpha * (data[pos(width, x-1, y)] + data[pos(width, x+1, y)] + data[pos(width, x, y-1)] + data[pos(width, x, y+1)]) + (1 - 4 * alpha) * data[pos(width, x, y)];
 }
 
-// Function to apply stencil to all data
 void apply_stencil(float* data, size_t width, size_t height, size_t offset, float alpha) {
-    #pragma omp parallel for
-    for (size_t y = 1; y < height - 1; y++) {
-        for (size_t x = 1; x < width - 1; x++) {
-            data[pos(width, x, y) + offset] = stencil(data, width, x, y, alpha);
+    (void)offset;
+    unsigned long x;
+    unsigned long y;
+    for (x=1; x < width-1; ++x){
+        for (y=1; y < height-1; ++y){
+            data[pos(width, x, y)] = stencil(data, width, x, y, alpha);
         }
     }
 }
 
-// Function to compute delta
 float compute_delta(float* data, float* prev, size_t width, size_t height) {
+    unsigned long i;
+    unsigned long j;
     float delta = 0.0f;
-    #pragma omp parallel for 
-    for (size_t y = 1; y < height - 1; y++) {
-        for (size_t x = 1; x < width - 1; x++) {
-            float diff = data[pos(width, x, y)] - prev[pos(width, x, y)];
-            delta += diff * diff;
+    for (i=1; i < width-1; ++i){
+        for (j=1; j < height-1; ++j){
+            delta += fabs(data[pos(width, i, j)] - prev[pos(width, i, j)]);
         }
     }
-    return delta/(width*height);
+    return delta;
 }
 
 void run_simulation(size_t width, size_t height, size_t steps, const char* filename) {
@@ -112,4 +107,3 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-
